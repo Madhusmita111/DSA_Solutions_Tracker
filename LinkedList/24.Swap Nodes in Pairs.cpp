589cd0478efeb699cb9e07f0cfbbb895f724cd1d
Madhusmita Talukdar <147142232+Madhusmita111@users.noreply.github.com>
2026-02-12 23:45:14 +0530

    Add iterative solution for swapping nodes in pairs
    Implemented a new solution for swapping nodes in pairs in a singly-linked list using an iterative approach with a dummy node.

---
+++ LinkedList/24.Swap Nodes in Pairs.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+
+//         if(!head || !head->next)return head;
+//         ListNode* first = head;
+//         ListNode* sec = head->next;
+//         first->next = swapPairs(sec->next);
+//         sec->next = first;
+
+//         return sec;
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        while(prev->next && prev->next->next){
+            ListNode* first = prev->next;
+            ListNode* sec = prev->next->next;
+
+            first->next = sec->next;
+            sec->next = first;
+            prev->next = sec;
+            prev = first;
+        }
+        return dummy->next;
+    }
+};
