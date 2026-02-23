89d175332134178afc79e8ac6a3aa853874f309a
Madhusmita Talukdar <147142232+Madhusmita111@users.noreply.github.com>
2026-02-23 23:43:55 +0530

    Create 347. Top K Frequent Elements.cpp

---
+++ Heap/347. Top K Frequent Elements.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(int a:nums){
+            mp[a]++;
+        }
+
+        vector<int>ans;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>minHeap;
+        for(auto it:mp){
+            minHeap.push({it.second,it.first});
+
+            if(minHeap.size()>k){
+                minHeap.pop();
+            }
+        }
+
+        while(!minHeap.empty()){
+            ans.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        return ans;
+    }
+};
